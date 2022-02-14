96374e1c9e907838c30cd70caf240086b5e012ff
Soranat Chamkring <66123671+soranatch4@users.noreply.github.com>
2022-02-15 01:01:58 +0700

    Update script.js

--- coursera_module4/script.js
+++ coursera_module4/script.js
@@ -1,8 +1,8 @@
 (function () {
 	var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 	for (var name in names) {
-	   var firstLetter = names[name].charAt(0).toLowerCase();
-	    if (firstLetter === 'j') {
+	   var firstCharacter = names[name].charAt(0).toLowerCase();
+	    if (firstCharacte === 'j') {
 	       byeSpeaker.speak(names[name]);
        } else {
 	 helloSpeaker.speak(names[name]);
