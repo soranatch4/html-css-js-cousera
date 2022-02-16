1f7485110dc7de06bbd76908b07cd4bdee52c981
Soranat Chamkring <66123671+soranatch4@users.noreply.github.com>
2022-02-16 17:56:26 +0700

    Delete SpeakHello.js

--- coursera_module4/SpeakHello.js
+++
@@ -1,8 +0,0 @@
-(function (window) {
-	var helloSpeaker = {};
-	var speakWord = "Hello";
-helloSpeaker.speak = function (name) {
-  console.log(speakWord + " " + name);
-}
-window.helloSpeak = helloSpeaker;
-})(window);
